a36f0ebf539603d1fbf22edd9d227e9900f336d3
Cameron Manderson <camm@flintinteractive.com.au>
2014-08-15 09:34:53 +1000

    Removes config check/set refs

--- GithubArticleServiceProvider.php
+++ GithubArticleServiceProvider.php
@@ -20,12 +20,10 @@ class GithubArticleServiceProvider implements ServiceProviderInterface
                     $app['config']['parameters']['github_repository'],
                     $app['config']['parameters']['github_reference']
                 );
-                if(!empty($app['config']['parameters']['github_auth_user_token']) && trim($app['config']['parameters']['github_auth_user_token']))
-                    $githubService->setAuthUserToken($app['config']['parameters']['github_auth_user_token']);
-                if(!empty($app['config']['parameters']['github_auth_user_token']) && trim($app['config']['parameters']['github_auth_method']))
-                    $githubService->setAuthMethod($app['config']['parameters']['github_auth_method']);
-                if(!empty($app['config']['parameters']['github_auth_user_token']) && trim($app['config']['parameters']['github_auth_password']))
-                    $githubService->setAuthMethod($app['config']['parameters']['github_auth_password']);
+                
+                $githubService->setAuthUserToken($app['config']['parameters']['github_auth_user_token']);
+                $githubService->setAuthMethod($app['config']['parameters']['github_auth_method']);
+                $githubService->setAuthMethod($app['config']['parameters']['github_auth_password']);
                 $githubService->setProvider($app['article_provider']);
 
                 return $githubService;
