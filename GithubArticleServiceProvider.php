20feec62f98196b6a9bfcd38efd130abb281b894
Cameron Manderson <camm@flintinteractive.com.au>
2014-08-15 08:38:38 +1000

    Adds a suggested Github Article Service Provider

---
+++ GithubArticleServiceProvider.php
@@ -0,0 +1,40 @@
+<?php
+/*
+ * Author; Cameron Manderson <cameronmanderson@gmail.com>
+ */
+namespace MMB\Github;
+
+use Silex\Application;
+use Silex\ServiceProviderInterface;
+
+class GithubArticleServiceProvider implements ServiceProviderInterface
+{
+    /**
+     * @param Application $app An Application instance
+     */
+    public function register(Application $app)
+    {
+        $app['article_service'] = $app->share(function ($app) {
+                $githubService = new \MMB\Github\GithubArticleService(
+                    $app['config']['parameters']['github_user'],
+                    $app['config']['parameters']['github_repository'],
+                    $app['config']['parameters']['github_reference']
+                );
+                $githubService->setAuthUserToken($app['config']['parameters']['github_auth_user_token']);
+                $githubService->setAuthMethod($app['config']['parameters']['github_auth_method']);
+                $githubService->setAuthMethod($app['config']['parameters']['github_auth_password']);
+                $githubService->setProvider($app['article_provider']);
+
+                return $githubService;
+            });
+
+    }
+
+    /**
+     */
+    public function boot(Application $app)
+    {
+
+    }
+
+}
