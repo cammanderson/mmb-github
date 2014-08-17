1929df5eae9ecfae5b36a90751c0ef2e8ff444e0
Cameron Manderson <camm@flintinteractive.com.au>
2014-08-18 07:02:53 +1000

    Updates use of contributors

--- GithubArticle.php
+++ GithubArticle.php
@@ -7,14 +7,16 @@ namespace MMB\Github;
 
 use MMB\AbstractArticle;
 use MMB\Meta\AuthoredInterface;
+use MMB\Meta\ContributedInterface;
 use MMB\Meta\MetaAuthorTrait;
+use MMB\Meta\MetaContributorsTrait;
 use MMB\Meta\MetaTimestampTrait;
 use MMB\Meta\MetaVersionTrait;
 use MMB\Meta\TimestampedInterface;
 use MMB\Meta\VersionedInterface;
 
-class GithubArticle extends AbstractArticle implements VersionedInterface, TimestampedInterface, AuthoredInterface
+class GithubArticle extends AbstractArticle implements VersionedInterface, TimestampedInterface, AuthoredInterface, ContributedInterface
 {
-    use MetaVersionTrait, MetaTimestampTrait, MetaAuthorTrait;
+    use MetaVersionTrait, MetaTimestampTrait, MetaAuthorTrait, MetaContributorsTrait;
 }
  
